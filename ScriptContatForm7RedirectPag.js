ba8dc302df0db5ccfd9c82c6c9b0958df2bd17c1
Maicon Bruno <maiconbrunorodrigues@gmail.com>
2020-01-14 10:13:51 -0300

    ScriptContactForm7

---
+++ ScriptContatForm7RedirectPag.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+    document.addEventListener( 'wpcf7mailsent', function( event ) {
+       location = 'https://www.hagana.com.br/agradecimento/';
+    }, false );
+    </script>
